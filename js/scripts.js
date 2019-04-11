fa52ee119b888efbd24b804a9fb43ef3bd354e89
Edilberto Espinosa <eespinosa57@misena.edu.co>
2019-04-11 18:51:18 -0500

    index construido
    Construción de la pagina de inicio

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*$(document).ready(function() {
+	$menu = $('#menu').find('ul').find('li');
+
+	$menu.hover(function() {
+		$(this).children('ul').stop();
+		$(this).children('ul').slideDown();
+	}, function() {
+		$(this).children('ul').stop();
+		$(this).children('ul').slideUp();
+	});
+});*/
+
